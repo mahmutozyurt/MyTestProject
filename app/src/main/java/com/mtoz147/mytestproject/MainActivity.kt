93116be18e6f9836dc3c15c195fb3b3c8b709c00
Mahmut OZYURT <mtoz147@gmail.com>
2023-09-03 22:51:18 +0300

    for loop added

---
+++ app/src/main/java/com/mtoz147/mytestproject/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mtoz147.mytestproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mylist= arrayListOf(10,20,30)
+        for (num in mylist){
+            println(num)
+        }
+    }
+}
